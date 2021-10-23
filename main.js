e5db561dace493c8ac3ad935889df1e8fc9e607e
dhiaan <82027019+dhiaan@users.noreply.github.com>
2021-10-23 16:14:23 +0845

    Add files via upload

--- main.js
+++ main.js
@@ -11,43 +11,33 @@ poseNet.on('pose',gotPoses);
 function modelLoaded() {
     console.log('poseNet Is Initialized!');
 }
-function gotPoses(results)
-{
-    if(results.length > 0)
-    {
-        console.log(results);
-    }
-}
-
 noseX=0;
 noseY=0;
 difference = 0;
 rightWristX = 0;
 leftWristX = 0;
-
 function gotPoses(results)
 {
-    if(results.length > 0)
-    {
-      console.log(results);
-      noseX = results[0].pose.nose.x;
-      noseY = results[0].pose.nose.y;
-      console.log("noseX =" + noseX +" noseY = " + noseY);
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +" noseY = " + noseY);
 
-      leftWristX = results[0].pose.leftWrist.x;
-      rightWristX = results[0].pose.rightWrist.x;
-      difference = floor( leftWristX - rightWristX );
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
 
-      console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
-     }
-    }
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
+}
 
-function draw () {
-  background('#969A97');
-  document.getElementById("square_side").innerHTML = "Width AND HEIGHT OF A SQUARE WILL BE = " + difference +"px"
+function draw() {
+background('#969A97');
+
+  document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
   fill('#F90093');
   stroke('#F90093');
   square(noseX, noseY, difference);
 }
-
-
