62cc9efcdb7a4d876441ab87fbcc4c6d0eedc893
dhiaan <82027019+dhiaan@users.noreply.github.com>
2021-10-23 16:06:41 +0845

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,35 @@ function gotPoses(results)
     }
 }
 
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+      console.log(results);
+      noseX = results[0].pose.nose.x;
+      noseY = results[0].pose.nose.y;
+      console.log("noseX =" + noseX +" noseY = " + noseY);
+
+      leftWristX = results[0].pose.leftWrist.x;
+      rightWristX = results[0].pose.rightWrist.x;
+      difference = floor( leftWristX - rightWristX );
+
+      console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+     }
+    }
+
+function draw () {
+  background('#969A97');
+  document.getElementById("square_side").innerHTML = "Width AND HEIGHT OF A SQUARE WILL BE = " + difference +"px"
+  fill('#F90093');
+  stroke('#F90093');
+  square(noseX, noseY, difference);
+}
+
+
